0acf5634ae137a6cc182ad18e4605b05a0e015a6
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-03-19 19:07:57 +0800

    Add header, footer, and partners section

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,75 @@
-import Image from "next/image";
+import Partners from "@/components/partners"
+
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <section className="max-w-7xl mx-auto text-center mb-8 px-4">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Hero Section Title</h1>
+        <p className="text-lg text-gray-600 leading-relaxed max-w-3xl mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla voluptates,
+          tempora beatae iusto, doloremque voluptatum dolorum molestiae odio consectetur
+        </p>
+        <div className="mt-6">
+          <button className="px-6 py-3 bg-[#f3dc8a] text-[#212529] font-bold rounded-lg shadow-md hover:bg-[#E9C235] transition cursor-pointer">
+            Download App
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </section>
+
+      <section className="max-w-7xl mx-auto px-4 my-10">
+        <Partners />
+      </section>
+
+      <section className="max-w-7xl mx-auto">
+        <h2 className="font-bold text-2xl mb-4">Players</h2>
+        <div className="grid gap-4 w-full max-w-7xl mx-auto grid-cols-1 md:grid-cols-3">
+          <div className="flex border p-4 w-[600px] h-[300px]">
+            {/* Image Section */}
+            <div className="flex-shrink-0 w-[40%] h-full border">
+              <Image
+                src="/img/players/player-one.webp"
+                className="h-full w-full object-contain"
+                width={349}
+                height={280}
+                alt="Player Image"
+              />
+            </div>
+
+            {/* Player Details Section */}
+            <div className="flex flex-col justify-center pl-4 w-[60%] self-end">
+              <span className="text-lg font-handwritten">Captain</span>
+              <h3 className="font-bold text-2xl font-handwritten">Player Name</h3>
+
+              {/* Stats Section */}
+              <div className="flex items-center text-lg font-handwritten mt-4">
+                <div className="text-center mx-2">
+                  <p>Matches</p>
+                  <p className="font-bold text-xl">12</p>
+                </div>
+                <span className="text-2xl mx-1">/</span>
+                <div className="text-center mx-2">
+                  <p>Runs</p>
+                  <p className="font-bold text-xl">12</p>
+                </div>
+                <span className="text-2xl mx-1">/</span>
+                <div className="text-center mx-2">
+                  <p>Highscore</p>
+                  <p className="font-bold text-xl">12</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex border p-4 w-[600px] h-[300px]"></div>
+          <div className="bg-gray-200 h-24 border border-black"></div>
+
+          <div className="col-span-3 flex justify-center gap-4">
+            <div className="bg-gray-200 h-24 w-1/4 border border-black"></div>
+            <div className="bg-gray-200 h-24 w-1/4 border border-black"></div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-[url(/bg-h&f.svg)] bg-center py-10">
+            <div className="max-w-7xl mx-auto">
+                <p className="text-white">© Copyright 2025 Gujarat Titans. All Rights Reserved.</p>
+            </div>
+        </footer>
+    )
+}
